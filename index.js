733f79c00ae04fc1e389e2719ffe32d9b0e6de66
daycool <qmw920@163.com>
2016-06-22 18:00:58 +0800

    添加文件

---
+++ index.js
@@ -0,0 +1,118 @@
+'use strict';
+
+var path = require('path'),
+	gutil = require('gulp-util'),
+	consolidate = require('consolidate'),
+	_ = require('lodash'),
+	Stream = require('stream');
+
+var PLUGIN_NAME  = 'gulp-iconfont-template';
+
+function iconfontHtml(config) {
+	var glyphMap = [],
+		currentGlyph,
+		currentCodePoint,
+		inputFilePrefix,
+		stream,
+		outputFile,
+		engine,
+		cssClass;
+
+	// Set default values
+	config = _.merge({
+		path: 'css',
+		targetPath: '_icons.css',
+		fontPath: './',
+		engine: 'lodash',
+		firstGlyph: 0xE001,
+		cssClass: 'icon'
+	}, config);
+
+	// Enable default stylesheet generators
+	if(!config.path) {
+		config.path = 'scss';
+	}
+	if(/^(scss|less|css)$/i.test(config.path)) {
+		config.path = __dirname + '/templates/_icons.' + config.path;
+	}
+
+	try {
+		engine = require(config.engine);
+	} catch(e) {
+		throw new gutil.PluginError(PLUGIN_NAME, 'Template engine "' + config.engine + '" not present');
+	}
+
+	// Define starting point
+	currentGlyph = config.firstGlyph;
+
+	// Happy streaming
+	stream = Stream.PassThrough({
+		objectMode: true
+	});
+
+	stream._transform = function(file, unused, cb) {
+		var fileName;
+
+		if (file.isNull()) {
+			this.push(file);
+			return cb();
+		}
+
+		// Create output file
+		if (!outputFile) {
+			outputFile = new gutil.File({
+				base: file.base,
+				cwd: file.cwd,
+				path: path.join(file.base, config.targetPath),
+				contents: file.isBuffer() ? new Buffer(0) : new Stream.PassThrough()
+			});
+		}
+
+		fileName = path.basename(file.path, '.svg');
+		glyphMap.push({
+			fileName: fileName,
+			// codePoint: currentCodePoint
+		});
+
+		file.path = path.dirname(file.path) + '/' + path.basename(file.path);
+
+		this.push(file);
+
+		cb();
+	};
+
+	stream._flush = function(cb) {
+		var content;
+		if (glyphMap.length) {
+			consolidate[config.engine](config.path, {
+					glyphs: glyphMap,
+					fontName: config.fontName,
+					fontPath: config.fontPath,
+					cssClass: config.cssClass
+				}, function(err, html) {
+					if (err) {
+						throw new gutil.PluginError(PLUGIN_NAME, 'Error in template: ' + err.message);
+					}
+
+					content = Buffer(html);
+
+					if (outputFile.isBuffer()) {
+						outputFile.contents = content;
+					} else {
+						outputFile.contents.write(content);
+						outputFile.contents.end();
+					}
+
+					stream.push(outputFile);
+
+					cb();
+			});
+		} else {
+			cb();
+		}
+	};
+
+	return stream;
+};
+
+module.exports = iconfontHtml;
